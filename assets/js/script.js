e420a9c52ab27e173088342b3f8907ed2d267bc8
johnnydwtt <johnnydwtt@gmail.com>
2021-08-19 16:43:05 +0200

    ajout de quelques modif

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,87 @@
-// Récupération des données du fichier json local
+var fnMouse = (jsonProducts) => {
+    jsonProducts.products.souris.map((infos) => {
+
+    let denomination = infos.product_denomination;
+    let picture = infos.product_picture;
+    let price = infos.product_price;
+
+    let itemHoardings = `
+        <div class='col-12 col-lg-4 text-center mbModif'>
+
+            <h3>${denomination}</h3>
+            <img class='img-fluid' src='${picture}'/>
+            <h3>${price}€</h3>
+
+            <div>
+            <span class="btn effect01">Ajouter au panier</span>
+            </div>
+
+        </div>`;
+    console.log(itemHoardings)
+    document.getElementById('mouse').innerHTML += itemHoardings;
+
+})  
+}
+
+
+let fnKeyboard = (jsonProducts) => {
+    jsonProducts.products.clavier.map((infos) => {
+
+        let denomination = infos.product_denomination;
+        let picture = infos.product_picture;
+        let price = infos.product_price;
+
+        let itemHoardings = `
+            <div class='col-12 col-lg-4 text-center mbModif'>
+
+                <h3>${denomination}</h3>
+                <img class='img-fluid' src='${picture}'/>
+                <h3>${price}€</h3>
+
+                <div>
+                <span class="btn effect01">Ajouter au panier</span>
+                </div>
+
+            </div> `;
+        console.log(itemHoardings)
+        document.getElementById('keyboard').innerHTML += itemHoardings;
+
+})
+}
+
+let fnScreen = (jsonProducts) => {
+    jsonProducts.products.ecran.map((infos) => {
+
+        let denomination = infos.product_denomination;
+        let picture = infos.product_picture;
+        let price = infos.product_price;
+
+        let itemHoardings = `
+            <div class='col-12 col-lg-4 text-center mbModif'>
+
+                <h3>${denomination}</h3>
+                <img class='img-fluid' src='${picture}'/>
+                <h3>${price}€</h3>
+
+                <div>
+                <span class="btn effect01">Ajouter au panier</span>
+                </div>
+
+            </div>`;
+        console.log(itemHoardings)
+        document.getElementById('screen').innerHTML += itemHoardings;
+
+})
+}
+
 fetch('assets/data/infosProducts.json')
     .then(response => response.json())
     .then((jsonProducts) => {
         console.log(jsonProducts)
-        // infosProducts correspond au nom du tableau json
-        jsonProducts.products.souris.map((infos) => {
-
-            let denomination = infos.product_denomination;
-            let picture = infos.product_picture;
-            let price = infos.product_price;
-
-            let itemHoardings = `
-                <div class='col-12 col-lg-4 text-center mbModif'>
-
-                    <h3>${denomination}</h3>
-                    <img class='img-fluid' src='${picture}'/>
-                    <h3>${price}€</h3>
-
-                    <div>
-                    <span class="btn effect01">Ajouter au panier</span>
-                    </div>
-
-                </div>`;
-            console.log(itemHoardings)
-            document.getElementById('mouse').innerHTML += itemHoardings;
+        // factorisation des fonctions
+        fnMouse(jsonProducts)
+        fnKeyboard(jsonProducts)
+        fnScreen(jsonProducts)
 
         })
-        jsonProducts.products.clavier.map((infos) => {
-
-            let denomination = infos.product_denomination;
-            let picture = infos.product_picture;
-            let price = infos.product_price;
-
-            let itemHoardings = `
-                <div class='col-12 col-lg-4 text-center mbModif'>
-
-                    <h3>${denomination}</h3>
-                    <img class='img-fluid' src='${picture}'/>
-                    <h3>${price}€</h3>
-
-                    <div>
-                    <span class="btn effect01">Ajouter au panier</span>
-                    </div>
-
-                </div> `;
-            console.log(itemHoardings)
-            document.getElementById('keyboard').innerHTML += itemHoardings;
-
-        })
-        jsonProducts.products.ecran.map((infos) => {
-
-            let denomination = infos.product_denomination;
-            let picture = infos.product_picture;
-            let price = infos.product_price;
-
-            let itemHoardings = `
-                <div class='col-12 col-lg-4 text-center mbModif'>
-
-                    <h3>${denomination}</h3>
-                    <img class='img-fluid' src='${picture}'/>
-                    <h3>${price}€</h3>
-
-                    <div>
-                    <span class="btn effect01">Ajouter au panier</span>
-                    </div>
-
-                </div>`;
-            console.log(itemHoardings)
-            document.getElementById('screen').innerHTML += itemHoardings;
-
-        })
-    })
+// Récupération des données du fichier json local
