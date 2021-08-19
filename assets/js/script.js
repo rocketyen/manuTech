dd82f1fc99cd1c6c80099e5adc61224c85300f75
rocketyen <rocketyen@gmail.com>
2021-08-19 16:42:33 +0200

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,9 +36,6 @@ fetch('assets/data/infosProducts.json')
                 <div class='col-12 col-lg-4 text-center mbModif'>
 
                     <h3>${denomination}</h3>
-                    <h4>${reference}</h4>
-                    <h4>${price}</h4>
-
                     <img class='img-fluid' src='${picture}'/>
                     <h3>${price}€</h3>
 
@@ -52,25 +49,92 @@ fetch('assets/data/infosProducts.json')
 
         })
         jsonProducts.products.ecran.map((infos) => {
-
+            let reference = infos.product_reference;
             let denomination = infos.product_denomination;
             let picture = infos.product_picture;
             let price = infos.product_price;
 
             let itemHoardings = `
                 <div class='col-12 col-lg-4 text-center mbModif'>
-
+                    <h2>${reference}</h2>
                     <h3>${denomination}</h3>
                     <img class='img-fluid' src='${picture}'/>
                     <h3>${price}€</h3>
 
                     <div>
-                    <span class="btn effect01">Ajouter au panier</span>
+                    <span class="btn effect01" id="add-to-cart">Ajouter au panier</span>
                     </div>
 
                 </div>`;
             console.log(itemHoardings)
             document.getElementById('screen').innerHTML += itemHoardings;
 
+            // let addItem = () => {
+            //     let denomination = document.getElementById('denomination');
+            //     if(denomination.value){
+            //         let item = [denomination.value];
+            //         items.push(item);
+            //         displayItems();
+            //         denomination.value = '';
+            //         localStorage.setItem('items', JSON.stringify(items))
+            //     }
+            // }
+            // let addBtn = document.getElementById('add-to-cart');
+            // addBtn.onclick = addItem;
+            // console.log(items);
+            
+            var shoppingCart = (function() {
+
+            function Item(reference, denomination, picture, price, count) {
+                this.reference = reference;
+                this.denomination = denomination;
+                this.picture = picture;
+                this.price = price;
+                this.count = count;
+              }
+
+            let cart = [];
+            function saveCart() {
+                localStorage.setItem('shoppingCart', JSON.stringify(cart));
+              }
+
+            var obj = {};
+            obj.addItemToCart = function(reference, denomination, price, count) {
+                for(var item in cart) {
+                  if(cart[item].denomination === denomination) {
+                    cart[item].count ++;
+                    saveCart();
+                    return;
+                  }
+                }
+                var item = new Item(reference, denomination, price, count);
+                cart.push(item);
+                saveCart();
+              }               
+            })(); 
+            $('.add-to-cart').click(function(event) {
+                event.preventDefault();
+                var denomination = $(this).data('denomination');
+                var price = Number($(this).data('price'));
+                shoppingCart.addItemToCart(denomination, price, 1);
+                displayCart();
+              });            
         })
     })
+
+    // function saveCart() {
+    //     localStorage.setItem('shoppingCart', JSON.stringify(cart));
+    //   }
+
+    // let cart = [];    
+
+    // function addProductToCart(denomination) {
+    //         for(var product_reference in cart) {
+    //         if(cart[product_reference].denomination === denomination) {
+    //             cart[product_reference].count ++;
+    //             saveCart();
+    //             return;
+    //         }
+    //         }
+    //     }
+       
