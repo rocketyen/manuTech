c46f57692300c4cefc020798df50e43fc6786b57
johnnydwtt <johnnydwtt@gmail.com>
2021-08-19 12:22:31 +0200

    ajout du Html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,31 +3,71 @@ fetch('assets/data/infosProducts.json')
     .then(response => response.json())
     .then((jsonProducts) => {
         console.log(jsonProducts)
-// infosProducts correspond au nom du tableau json
-        jsonProducts.products.map((infos) => {
+        // infosProducts correspond au nom du tableau json
+        jsonProducts.products.souris.map((infos) => {
 
-            let reference = infos.product_reference;
             let denomination = infos.product_denomination;
-            let description = infos.product_description;
             let picture = infos.product_picture;
             let price = infos.product_price;
-            
+
             let itemHoardings = `
-            <div class='col-12 col-lg-4'>     
-                <div class='col-6'>
-                    <img class='img-fluid' src='${picture}'/>
-                </div>
-                <div class='col-6'>
+                <div class='col-12 col-lg-4 text-center mbModif'>
+
                     <h3>${denomination}</h3>
-                    <h4>${reference}</h4>
-                    <p>${description}</p>
-                    <h4>${price}</h4>
-                </div>
-                
-            </div>
-            `;
+                    <img class='img-fluid' src='${picture}'/>
+                    <h3>${price}€</h3>
+
+                    <div>
+                    <span class="btn effect01">Ajouter au panier</span>
+                    </div>
+
+                </div>`;
             console.log(itemHoardings)
-            document.getElementById('hoarding').innerHTML += itemHoardings;
-          
-      })
+            document.getElementById('mouse').innerHTML += itemHoardings;
+
+        })
+        jsonProducts.products.clavier.map((infos) => {
+
+            let denomination = infos.product_denomination;
+            let picture = infos.product_picture;
+            let price = infos.product_price;
+
+            let itemHoardings = `
+                <div class='col-12 col-lg-4 text-center mbModif'>
+
+                    <h3>${denomination}</h3>
+                    <img class='img-fluid' src='${picture}'/>
+                    <h3>${price}€</h3>
+
+                    <div>
+                    <span class="btn effect01">Ajouter au panier</span>
+                    </div>
+
+                </div> `;
+            console.log(itemHoardings)
+            document.getElementById('keyboard').innerHTML += itemHoardings;
+
+        })
+        jsonProducts.products.ecran.map((infos) => {
+
+            let denomination = infos.product_denomination;
+            let picture = infos.product_picture;
+            let price = infos.product_price;
+
+            let itemHoardings = `
+                <div class='col-12 col-lg-4 text-center mbModif'>
+
+                    <h3>${denomination}</h3>
+                    <img class='img-fluid' src='${picture}'/>
+                    <h3>${price}€</h3>
+
+                    <div>
+                    <span class="btn effect01">Ajouter au panier</span>
+                    </div>
+
+                </div>`;
+            console.log(itemHoardings)
+            document.getElementById('screen').innerHTML += itemHoardings;
+
+        })
     })
